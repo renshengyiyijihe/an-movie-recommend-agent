32361894e4482f0a055fec18982b6d101090fc8c
yangjinhu <1191681452@qq.com>
2026-08-12 16:28:47 +0800

    feat: 更换模型

--- backend/movie-service/src/model/model.provider.ts
+++ backend/movie-service/src/model/model.provider.ts
@@ -116,7 +116,7 @@ export class ModelProvider {
     const baseURL = "https://integrate.api.nvidia.com/v1";
     // const modelName = "minimaxai/minimax-m3";
     // const modelName = "z-ai/glm-5.2"
-    const modelName = "meta/llama-guard-4-12b";
+    const modelName = "mistralai/mistral-nemotron";
     const temperature = process.env.NVIDIA_TEMPERATURE
       ? Number(process.env.NVIDIA_TEMPERATURE)
       : 0.3;
